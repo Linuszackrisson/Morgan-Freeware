644c1834bb78cad9db10c7f59f832d7a0c0a50fc
Linuszackrisson <linus.gz@hotmail.com>
2025-03-14 00:51:02 +0100

    tjenare

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const config: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
@@ -7,4 +8,4 @@ const nextConfig = {
   basePath: '/Morgan-Freeware'
 }
 
-module.exports = nextConfig
+export default config
