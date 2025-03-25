ded60f055336feb82a524aac30ef4ed944f9c0e2
Linus Zackrisson <linus.gz@hotmail.com>
2025-03-25 02:51:13 +0100

    newsletter comments

--- src/utils/newsletter-service.ts
+++ src/utils/newsletter-service.ts
@@ -3,18 +3,18 @@ import { supabase } from '@/utils/supabase';
 export async function subscribeToNewsletter(email: string) {
   
     const { data: existingEmail } = await supabase
-    .from("newsletter")
-    .select("email")
-    .eq("email", email)
-    .single();
+    .from("newsletter") // vi hämtar vår tabell
+    .select("email") // vi hämtar emailen från vår tabell
+    .eq("email", email) // vi jämför emailen med vår input
+    .single(); // vi vill ha en enda rad så vi kan jämföra med vår input
 
   if (existingEmail) {
     throw new Error("This email is already subscribed");
   }
 
   const { error } = await supabase
-    .from("newsletter")
-    .insert([{ email }]);
+    .from("newsletter") // vi hämtar vår tabell
+    .insert([{ email }]); // vi skickar in vår input jippie
 
   if (error) {
     console.error('Supabase error:', error);
