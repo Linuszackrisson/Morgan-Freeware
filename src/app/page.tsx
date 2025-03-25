fc137e999531feb7287716645060788b085486fb
Linus Zackrisson <linus.gz@hotmail.com>
2025-03-25 02:47:48 +0100

    comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import NewsletterForm from '@/components/Newsletter';
 import TestimonialSlider from '@/components/TestimonialSlider';
 import ContactForm from '@/components/ContactForm';
 
-export default async function Home() {
-  const software = await getAllSoftware();
+export default async function Home() { // Vi använder async för att hämta våra data från vår backend
+  const software = await getAllSoftware(); // hämtar ALLA våra program, som vi skickar till våran grid komponent på huvudsidan
 
   return (
     <main className="w-full min-h-screen relative bg-white">
